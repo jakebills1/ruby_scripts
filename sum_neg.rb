2072685b3635f6a81f87950f502c243f3c90a590
Jake Bills <jakebills1@gmail.com>
2019-05-22 09:05:14 -0600

    adds sum_neg.rb

--- sum_neg.rb
+++ sum_neg.rb
@@ -1,3 +1,18 @@
 def sum_neg(arr)
-	
+    results = []
+    if arr != []
+        results = [0, 0]
+        for n in arr
+            if n > 0 
+                results[0] += 1
+            else
+                results[1] += n 
+            end
+        end
+    end
+    return results
 end
+
+# test case
+puts sum_neg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+puts sum_neg([]) 
